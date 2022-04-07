90c809f3e8b41d64d0f9b847df558e7a0cf069db
Pahey420 <102471625+Pahey420@users.noreply.github.com>
2022-04-07 23:58:42 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,13 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+numbers = ['2', '-4', '5', '0', '-7', '4', '2']
+count = []
+for number in numbers:
+    number = int(number)
+    num = 0
+    if number > 0 and number % 2 == 0:
+        num += number
+        count.append(num)
+num = int(num)
+print(sum(count))

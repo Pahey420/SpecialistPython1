2dde80a7fb3380a3e304d3744902370b2ea6bb87
Pahey420 <102471625+Pahey420@users.noreply.github.com>
2022-04-23 23:37:16 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -2,4 +2,21 @@
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
 with open("data/info.txt", "r") as f:
-    pass
+    positive_number = 0
+    negative_counter = 0
+    negative_number = 0
+    for positive_counter in f:
+        positive_counter = positive_counter.strip()  # удаляем все лишние переносы строк
+        if positive_counter.isnumeric():
+            positive_number += int(positive_counter)
+            # print(positive_number)   # вывести положительное число
+    f.seek(0)
+    for negative_counter in f:
+        negative_counter = negative_counter.strip()
+        if negative_counter.find("-") == 0:
+            negative_counter = negative_counter.replace("-", "")
+            if negative_counter.isdecimal():
+                negative_number += int(negative_counter)
+    # print(negative_number)     # вывести положительное от отрицального число
+print("totl: ", positive_number - negative_number)
+pass
